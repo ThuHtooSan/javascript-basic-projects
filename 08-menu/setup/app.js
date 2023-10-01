093453644cf3794a339eec40cbf97b5d55d9092d
Thu Htoo San <43321350+ThuHtooSan@users.noreply.github.com>
2023-10-01 20:48:17 +0630

    Finish menu project

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -1,6 +1,5 @@
 const menu = [
   {
-    id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
     price: 15.99,
@@ -8,7 +7,6 @@ const menu = [
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
-    id: 2,
     title: "diner double",
     category: "lunch",
     price: 13.99,
@@ -16,7 +14,6 @@ const menu = [
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
-    id: 3,
     title: "godzilla milkshake",
     category: "shakes",
     price: 6.99,
@@ -24,7 +21,6 @@ const menu = [
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
-    id: 4,
     title: "country delight",
     category: "breakfast",
     price: 20.99,
@@ -32,7 +28,6 @@ const menu = [
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
-    id: 5,
     title: "egg attack",
     category: "lunch",
     price: 22.99,
@@ -40,7 +35,6 @@ const menu = [
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
-    id: 6,
     title: "oreo dream",
     category: "shakes",
     price: 18.99,
@@ -48,7 +42,6 @@ const menu = [
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
-    id: 7,
     title: "bacon overflow",
     category: "breakfast",
     price: 8.99,
@@ -56,19 +49,79 @@ const menu = [
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
-    id: 8,
     title: "american classic",
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
-  {
-    id: 9,
+  { 
     title: "quarantine buddy",
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    title: "bison steak",
+    category: "dinner",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
+
+const filterContainer = document.querySelector('#filter-container');
+const menuContainer = document.querySelector('#menu-container');
+
+function createFilterBtns() {
+  // collect unique categories
+  const categories = menu.reduce((result, { category }) => {
+    if (!result.includes(category)) {
+      result.push(category);
+    }
+    return result;
+  }, ['all']);
+
+  // clear buttons
+  filterContainer.innerHTML = "";
+
+  // append buttons to filter container
+  categories.forEach(category => {
+    const btn = document.createElement('button');
+    btn.classList.add('filter-btn');
+    btn.textContent = category;
+    btn.addEventListener('click', () => displayMenuItem(category));
+
+    filterContainer.appendChild(btn);
+  });
+}
+
+function displayMenuItem(category) {
+  let menuItems;
+  
+  menuItems = category === "all"
+    ? menu
+    : menu.filter(menuItem => menuItem.category === category);
+
+  const itemElems = menuItems.map(menuItem => 
+    `<section class="menu-item">
+      <div class="image-container">
+        <img src=${menuItem.img} alt=${menuItem.title}/>
+      </div>
+      <div class="content">
+        <div class="name">${menuItem.title}</div>
+        <p class="description">${menuItem.desc}</p>
+        <p class="price">$${menuItem.price}</p>
+      </div>
+    </section>`
+  );
+
+  menuContainer.innerHTML = itemElems.join('');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayMenuItem('all');
+  createFilterBtns();
+})
+
