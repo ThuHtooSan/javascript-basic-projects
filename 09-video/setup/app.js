a12f30090550603b89b38698e5603fe7b37c77f1
Thu Htoo San <43321350+ThuHtooSan@users.noreply.github.com>
2023-10-02 18:04:15 +0630

    Finish video project

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,23 @@
-// MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+const preloader = document.querySelector('#preloader');
+const controlBtn = document.querySelector('.playback-controls #controlBtn');
+const video = document.querySelector('.video-container');
+
+// remove the preloader after window loaded
+window.addEventListener('load', () => preloader.classList.add('hide'));
+
+// play/pause the video & change the icon accordingly
+controlBtn.parentElement.addEventListener('click', () => {
+    if (!controlBtn.classList.contains('paused')) {
+        controlBtn.classList.add('paused');
+        video.pause();
+        setTimeout(() => 
+            controlBtn.innerHTML = `<i class="fa-solid fa-play"></i>`
+        , 200);
+    } else {
+        controlBtn.classList.remove('paused');
+        video.play()
+        setTimeout(() => 
+            controlBtn.innerHTML = `<i class="fa-solid fa-pause"></i>`
+        , 200);
+    }
+})
